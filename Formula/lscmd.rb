ce076ef89824ebbd65768a6d1cc1eb99124a9a95
JackaL <shiveringbulbul@hotmail.com>
2025-09-04 22:54:08 +0800

    feat(formula): update lscmd to v0.2.0

--- Formula/lscmd.rb
+++ Formula/lscmd.rb
@@ -1,8 +1,8 @@
 class Lscmd < Formula
   desc "Shell command visualization tool - scan and display shell aliases and functions"
   homepage "https://github.com/monshakeys/lscmd"
-  url "https://github.com/monshakeys/lscmd/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "598e0e47946ed72278c8b165db3ea60282d1980f7a83de4cb6b876dec644ae6d"
+  url "https://github.com/monshakeys/lscmd/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "dafc46814bfe6f7d974e57d6e3ed44540468db72844b51189b8c6f4b055e7de5"
   license "MIT"
 
   depends_on "rust" => :build
